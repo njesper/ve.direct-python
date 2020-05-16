e2cf4a5047501b8f5a148889dc8b76c4cc18e118
Bjoern Riemer <bjoern.riemer@fokus.fraunhofer.de>
2020-05-17 00:48:04 +0200

    add options for print only and made send interval configurable

--- vedirect/__main__.py
+++ vedirect/__main__.py
@@ -11,6 +11,7 @@ from influxdb import InfluxDBClient
 influx_client = None
 influx_db = None
 lastsend = datetime.datetime.min
+args = None
 
 def main():
     """
@@ -18,30 +19,38 @@ def main():
     :return:
     """
     print("Startup...")
+    global args
     parser = argparse.ArgumentParser(description='Parse VE.Direct serial data')
     parser.add_argument('-i', '--influx', help='Influx DB host')
     parser.add_argument('-d', '--database', help='InfluxDB database')
     parser.add_argument('-p', '--port', help='Serial port')
+    parser.add_argument('-s', '--send-interval', help='send interval seconds',default=5, type=int)
+    parser.add_argument('-n', '--print-only', help='dont send anything to influxdb, just print', action='store_true')
     args = parser.parse_args()
 
-    global influx_db, influx_client
-    influx_client = InfluxDBClient(host=args.influx, port=8086)
-    influx_db = args.database
+    if not args.print_only:
+        global influx_db, influx_client
+        influx_client = InfluxDBClient(host=args.influx, port=8086)
+        influx_db = args.database
 
     ve = Vedirect(args.port)
     ve.read_data_callback(on_victron_data_callback)
 
+def mean(nums):
+    return sum(nums, 0.0) / len(nums)
+
 def on_victron_data_callback(data):
-    global lastsend
+    global lastsend, args
     diff = datetime.datetime.now()-lastsend
-    print(diff)
-    if diff.seconds > 30:
+    if diff.seconds > args.send_interval:
         measurements = influx.measurements_for_packet(data)
-        influx_client.write_points(measurements, database=influx_db)
+        if not args.print_only:
+            influx_client.write_points(measurements, database=influx_db)
         print(measurements)
         lastsend=datetime.datetime.now()
     else:
-        print ("Skipped measurements")
+        #print ("Skipped measurement")
+        pass
 
 
 if __name__ == "__main__":
